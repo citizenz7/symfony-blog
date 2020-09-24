f8c6bac599d596235434dc60ce4c863fdd881b9b
citizenz7 <citizenz7@protonmail.com>
2020-09-24 11:45:48 +0200

    pagination

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,13 +13,19 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(ArticleRepository $repo)
+    public function index(ArticleRepository $repo, Request $request, PaginatorInterface $paginator)
     {
-        $articles = $repo->findAll();
+        $donnees = $repo->findAll();
 
-        return $this->render('admin/index.html.twig', [
+        $articles = $paginator->paginate(
+            $donnees, // on passe les données
+            $request->query->getInt('page', 1), // N° de la page en cours, 1 par défaut
+            3 // nombre d'éléments par page
+        );
+
+        return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
-            'articles' => $articles
+            'articles' => $articles,
         ]);
     }
 
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,6 +8,7 @@ use App\Form\ArticleType;
 use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,13 +27,19 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(ArticleRepository $repo)
+    public function index(ArticleRepository $repo, Request $request, PaginatorInterface $paginator)
     {
-        $articles = $repo->findAll();
+        $donnees = $repo->findAll();
+
+        $articles = $paginator->paginate(
+            $donnees, // on passe les données
+            $request->query->getInt('page', 1), // N° de la page en cours, 1 par défaut
+            3 // nombre d'éléments par page
+        );
 
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
-            'articles' => $articles
+            'articles' => $articles,
         ]);
     }
 
